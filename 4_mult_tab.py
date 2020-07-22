117de2dcf55856eae04a1a748991168b0aa8d039
Aditya-0517 <57567973+Aditya-0517@users.noreply.github.com>
2020-07-22 15:23:42 +0530

    Update 4_mult_tab.py

--- 4_mult_tab.py
+++ 4_mult_tab.py
@@ -1,6 +1,6 @@
-num = input("\nmultiplication table of: ")
-integer = input("\nexpected till: ")
+num = int(input("\nmultiplication table of: "))
+integer = int(input("\nexpected till: "))
 for x in range (1, integer+1):
-    print(str(num) + "X" + str(x) + "=" + str(num*x))
+    print(num,"X",x,"=",num*x)
 
 
