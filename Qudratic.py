a7543318d070189bf1ba592a60540134881fbbf3
Het Patel <hetpokar@gmail.com>
2020-07-24 16:20:11 +0530

    added a == 0

--- Qudratic.py
+++ Qudratic.py
@@ -5,21 +5,25 @@ def quadratic():
     #discriminant
     D = ((b**2)-(4*a*c))
     #for real and distinct roots
-    if (D > 0) :
-        print ("Roots are real and distinct:")
-        root1 = (-b + math.sqrt(D)) / (2*a)
-        root2 = (-b - math.sqrt(D)) / (2*a)
-        print("Root 1 = {:.3f} and root 2 = {:.3f} ".format(root1,root2))
-    #for real and identical roots
-    elif D == 0 :
-        print("Roots are real and identical :")
-        root1 = -b / (2*a)
-        print("Root = {:.3f}".format(root1))
-    #for complex roots
-    elif D < 0 :
-        print("Roots are imaginary:")
-        rootreal = -b/(2*a)
-        rootimg = math.sqrt(-D)/(2*a)
-        print("Root 1 = {:.3f} + {:.3f}i and root 2 = {:.3f} - {:.3f}i".format(rootreal,rootimg,rootreal,rootimg))
+    if a == 0 :
+        print("IT is not a quadratic equation")
+        quadratic()
+    else:
+        if (D > 0) :
+            print ("Roots are real and distinct:")
+            root1 = (-b + math.sqrt(D)) / (2*a)
+            root2 = (-b - math.sqrt(D)) / (2*a)
+            print("Root 1 = {:.3f} and root 2 = {:.3f} ".format(root1,root2))
+        #for real and identical roots
+        elif D == 0 :
+            print("Roots are real and identical :")
+            root1 = -b / (2*a)
+            print("Root = {:.3f}".format(root1))
+        #for complex roots
+        elif D < 0 :
+            print("Roots are imaginary:")
+            rootreal = -b/(2*a)
+            rootimg = math.sqrt(-D)/(2*a)
+            print("Root 1 = {:.3f} + {:.3f}i and root 2 = {:.3f} - {:.3f}i".format(rootreal,rootimg,rootreal,rootimg))
 
 quadratic()
